dceba4fb1e6a1d7f59e7e3ffb48a929df005c95b
bizire <aa952393@gmail.com>
2023-07-20 11:52:40 +0300

    showToast

--- TestApp/Sources/AudioPlayer/FolderContentViewController.swift
+++ TestApp/Sources/AudioPlayer/FolderContentViewController.swift
@@ -45,6 +45,8 @@ class FolderContentViewController: UIViewController, UITableViewDataSource, UITa
         
         tableView.dataSource = self
         tableView.delegate = self
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
         print("folderNames folderName = \(folderName)")
         loadFolderFiles()
  
@@ -430,3 +432,24 @@ class FolderContentViewController: UIViewController, UITableViewDataSource, UITa
         label.text = title + " - " + NSLocalizedString("chapter", comment: "") + ": " + (AudioPlayerManager.shared.getFileName() as NSString).deletingPathExtension
     }
 }
+
+extension FolderContentViewController {
+
+func showToast(message : String, font: UIFont) {
+
+    let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-500, width: 150, height: 35))
+    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+    toastLabel.textColor = UIColor.white
+    toastLabel.font = font
+    toastLabel.textAlignment = .center;
+    toastLabel.text = message
+    toastLabel.alpha = 1.0
+    toastLabel.layer.cornerRadius = 10;
+    toastLabel.clipsToBounds  =  true
+    self.view.addSubview(toastLabel)
+    UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+         toastLabel.alpha = 0.0
+    }, completion: {(isCompleted) in
+        toastLabel.removeFromSuperview()
+    })
+} }
